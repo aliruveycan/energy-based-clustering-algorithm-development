58d6409c496ce186ca6bff98b03fb4fd7cb49a1a
Ali Rüveycan <113598241+aliruveycan@users.noreply.github.com>
2024-05-11 20:38:52 +0300

    energy based clustering algorithm development
    energy based clustering algorithm development

---
+++ deneme/deneme/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Node
+{
+    public int ID = -1; // Düğümün kimlik numarası, başlangıç değeri atanıyor
+    public double Energy = 100.0; // Düğümün mevcut enerji seviyesi, başlangıç değeri atanıyor
+}
+
+class Cluster
+{
+    public int ClusterID; // Kümenin kimlik numarası
+    public List<Node> Nodes; // Kümedeki düğümlerin listesi
+    public double ClusterEnergy; // Kümenin toplam enerji seviyesi
+
+    public Cluster(int id)
+    {
+        ClusterID = id;
+        Nodes = new List<Node>();
+        ClusterEnergy = 0;
+    }
+}
+
+class EnergyBasedClustering
+{
+    List<Node> nodes; // Tüm düğümlerin listesi
+    public List<Cluster> clusters; // Oluşturulan kümelerin listesi
+
+    public EnergyBasedClustering(List<Node> nodeList)
+    {
+        nodes = nodeList;
+        clusters = new List<Cluster>();
+    }
+
+    public void CreateClusters(int k)
+    {
+        // Kümelerin oluşturulması
+        for (int i = 0; i < k; i++)
+        {
+            Cluster cluster = new Cluster(i);
+            clusters.Add(cluster);
+        }
+
+        // Düğümlerin kümelere atanması
+        foreach (Node node in nodes)
+        {
+            Cluster nearestCluster = FindNearestCluster(node);
+            nearestCluster.Nodes.Add(node);
+            nearestCluster.ClusterEnergy += node.Energy;
+        }
+    }
+
+    private Cluster FindNearestCluster(Node node)
+    {
+        if (clusters.Count == 0)
+        {
+            // Eğer hiç küme yoksa, yeni bir küme oluşturulur
+            return new Cluster(0);
+        }
+
+        Cluster nearestCluster = clusters[0];
+        double minDistance = double.MaxValue; // Başlangıçta maksimum değer atanır
+
+        foreach (Cluster cluster in clusters)
+        {
+            if (cluster.Nodes.Count == 0)
+            {
+                // Eğer kümenin hiç düğümü yoksa, bu küme geçersizdir ve diğer kümelere bakılır
+                continue;
+            }
+
+            foreach (Node clusterNode in cluster.Nodes)
+            {
+                double distance = Distance(node, clusterNode);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestCluster = cluster;
+                }
+            }
+        }
+
+        return nearestCluster;
+    }
+
+    private double Distance(Node node1, Node node2)
+    {
+        // İki düğüm arasındaki mesafenin hesaplanması
+        return Math.Sqrt(Math.Pow((node1.ID - node2.ID), 2) + Math.Pow((node1.Energy - node2.Energy), 2));
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Örnek düğümler oluşturuluyor
+        List<Node> nodeList = new List<Node>
+        {
+            new Node { ID = 1, Energy = 50.0 },
+            new Node { ID = 2, Energy = 70.0 },
+            new Node { ID = 3, Energy = 80.0 }
+        };
+
+        // Enerji tabanlı kümeleme algoritması oluşturuluyor ve küme sayısı belirleniyor
+        EnergyBasedClustering clustering = new EnergyBasedClustering(nodeList);
+        clustering.CreateClusters(2);
+
+        // Sonuçlar ekrana yazdırılıyor
+        Console.WriteLine("Enerji tabanlı kümeleme algoritması başarıyla tamamlandı.");
+        Console.WriteLine("Oluşturulan kümeler:");
+
+        foreach (Cluster cluster in clustering.clusters)
+        {
+            Console.WriteLine($"Küme {cluster.ClusterID}: Toplam enerji = {cluster.ClusterEnergy}");
+
+            Console.WriteLine("Küme içeriği:");
+            foreach (Node node in cluster.Nodes)
+            {
+                Console.WriteLine($"Düğüm {node.ID}: Enerji = {node.Energy}");
+            }
+        }
+
+        // Sonucun görüntülenmesi için bir tuşa basılması bekleniyor
+        Console.WriteLine("Çıkmak için bir tuşa basınız...");
+        Console.ReadKey();
+    }
+}
---
+++ enerji_2/enerji_2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Node
+{
+    public int ID { get; set; }
+    public double Energy { get; set; }
+    public bool Clustered { get; set; }
+    public int ClusterID { get; set; }
+}
+
+class Cluster
+{
+    public int ID { get; set; }
+    public List<Node> Nodes { get; set; }
+}
+
+class LEACHAlgorithm
+{
+    private List<Node> nodes;
+    private List<Cluster> clusters;
+    private int round;
+
+    public LEACHAlgorithm(List<Node> nodeList)
+    {
+        nodes = nodeList;
+        clusters = new List<Cluster>();
+        round = 1;
+    }
+
+    public void Run(int k, double threshold)
+    {
+        while (nodes.Any(n => !n.Clustered))
+        {
+            FormClusters(k, threshold);
+            round++;
+            RenewClusterHead();
+        }
+        DisplayClusters(); // Sonuçları ekrana yazdır
+        Console.WriteLine("Çıkmak için bir tuşa basınız...");
+        Console.ReadKey(); // Kapatmak için bir tuşa basılmasını bekler
+    }
+
+    private void FormClusters(int k, double threshold)
+    {
+        clusters.Clear();
+        Random rnd = new Random();
+        for (int i = 0; i < k; i++)
+        {
+            int randomNodeIndex = rnd.Next(nodes.Count);
+            Node clusterHead = nodes[randomNodeIndex];
+            Cluster cluster = new Cluster { ID = i, Nodes = new List<Node> { clusterHead } };
+            clusterHead.Clustered = true;
+            clusterHead.ClusterID = i;
+            clusters.Add(cluster);
+        }
+
+        foreach (Node node in nodes)
+        {
+            if (!node.Clustered)
+            {
+                Cluster nearestCluster = null;
+                double minDistance = double.MaxValue;
+
+                foreach (Cluster cluster in clusters)
+                {
+                    double distance = Distance(node, cluster.Nodes[0]);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        nearestCluster = cluster;
+                    }
+                }
+
+                if (minDistance <= threshold)
+                {
+                    nearestCluster.Nodes.Add(node);
+                    node.Clustered = true;
+                    node.ClusterID = nearestCluster.ID;
+                }
+            }
+        }
+    }
+
+    private void RenewClusterHead()
+    {
+        foreach (Cluster cluster in clusters)
+        {
+            double minEnergy = double.MaxValue;
+            Node newClusterHead = null;
+            foreach (Node node in cluster.Nodes)
+            {
+                if (node.Energy < minEnergy)
+                {
+                    minEnergy = node.Energy;
+                    newClusterHead = node;
+                }
+            }
+
+            foreach (Node node in cluster.Nodes)
+            {
+                node.Clustered = false;
+            }
+
+            cluster.Nodes.Clear();
+            newClusterHead.Clustered = true;
+            newClusterHead.ClusterID = cluster.ID;
+            cluster.Nodes.Add(newClusterHead);
+        }
+    }
+
+    private double Distance(Node node1, Node node2)
+    {
+        return Math.Sqrt(Math.Pow((node1.ID - node2.ID), 2) + Math.Pow((node1.Energy - node2.Energy), 2));
+    }
+
+    public void DisplayClusters()
+    {
+        Console.WriteLine($"Round: {round}");
+        foreach (Cluster cluster in clusters)
+        {
+            Console.WriteLine($"Cluster {cluster.ID}:");
+            foreach (Node node in cluster.Nodes)
+            {
+                Console.WriteLine($"Node {node.ID}, Energy: {node.Energy}");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Node> nodeList = new List<Node>
+        {
+            new Node { ID = 1, Energy = 50 },
+            new Node { ID = 2, Energy = 70 },
+            new Node { ID = 3, Energy = 80 },
+            new Node { ID = 4, Energy = 60 },
+            new Node { ID = 5, Energy = 90 }
+        };
+
+        LEACHAlgorithm leach = new LEACHAlgorithm(nodeList);
+        leach.Run(2, 20);
+    }
+}
